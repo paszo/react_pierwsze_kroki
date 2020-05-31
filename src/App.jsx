03a98bec7f42dd154e4e8fcfca5ba07682d02617
paszo <paszo2008@gmail.com>
2020-05-31 07:37:54 +0200

    final

---
+++ src/App.jsx
@@ -0,0 +1,131 @@
+import React, {Component} from 'react';
+import uniqid from 'uniqid';
+import "./App.css";
+import Countdown from './Countdown';
+import EditEvent from "./EditEvent";
+
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            now: {
+                hour: new Date().getHours(),
+                minute: new Date().getMinutes(),
+                seconds: new Date().getSeconds(),
+            },
+            events: [],
+            editedEvent: {
+                id: uniqid(),
+                name: "",
+                hour: -1,
+                minute: -1
+            }
+        };
+
+        this.handleEditEvent = this.handleEditEvent.bind(this);
+        this.handleSaveEvent = this.handleSaveEvent.bind(this);
+        this.handleRemoveEvent = this.handleRemoveEvent.bind(this);
+        this.handleEditInit = this.handleEditInit.bind(this);
+        this.handleEditCancel = this.handleEditCancel.bind(this);
+        this.timer = this.timer.bind(this);
+    }
+
+    timer() {
+        this.setState({
+            now: {
+                hour: new Date().getHours(),
+                minute: new Date().getMinutes(),
+                seconds: new Date().getSeconds(),
+            }
+        })
+    }
+
+    componentDidMount() {
+        const storateEvents = JSON.parse(localStorage.getItem("events")) || [];
+        this.setState({events: storateEvents});
+        const intervalId = setInterval(this.timer, 1000);
+        this.setState({intervalId: intervalId});
+    }
+
+    handleEditEvent(val) {
+        this.setState(prevState => {
+            return {
+                editedEvent: Object.assign(prevState.editedEvent, val)
+            }
+        })
+    }
+
+    handleSaveEvent() {
+        this.setState(prevState => {
+            const editedEventExists = prevState.events.find( el => el.id === prevState.editedEvent.id);
+
+            let updatedEvents;
+            if(editedEventExists){
+                updatedEvents = prevState.events.map(el => {
+                    if (el.id === prevState.editedEvent.id) return prevState.editedEvent;
+                    else return el;
+                })
+            } else {
+                updatedEvents = [...prevState.events, prevState.editedEvent]
+            }
+
+            return {
+                events: updatedEvents,
+                editedEvent: {id: uniqid(), name: "", hour: -1, minute: -1}
+            }
+        }, () => {
+            localStorage.setItem("events", JSON.stringify(this.state.events))
+        });
+
+    }
+
+    handleRemoveEvent(id) {
+        this.setState(prevState => ({
+            events: prevState.events.filter(el => el.id !== id)
+        }), () => {
+            localStorage.setItem("events", JSON.stringify(this.state.events))
+        })
+    }
+
+    handleEditInit(id) {
+        this.setState(prevState => ({
+            editedEvent: {...prevState.events[id]}
+        }))
+    }
+
+    handleEditCancel(){
+        this.setState({
+            editedEvent: {id: uniqid(), name: "", hour: -1, minute: -1}
+        });
+    }
+
+    render() {
+        const events = this.state.events.map(el => {
+            return <Countdown
+                key={el.id}
+                id={el.id}
+                name={el.name}
+                hour={el.hour}
+                minute={el.minute}
+                timeNow={this.state.now}
+                onRemove={id => this.handleRemoveEvent(id)}
+                onEditInit={id => this.handleEditInit(id)}
+            />
+        });
+        return (
+            <div className="app">
+                {events}
+                <EditEvent
+                    name={this.state.editedEvent.name}
+                    hour={this.state.editedEvent.hour}
+                    minute={this.state.editedEvent.minute}
+                    onInputChange={val => this.handleEditEvent(val)}
+                    onSave={() => this.handleSaveEvent()}
+                    onCancel={() => this.handleEditCancel()}
+                />
+            </div>
+        )
+    }
+}
+
+export default App;
---
+++ src/Countdown.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import "./Countdown.css";
+import "./../node_modules/semantic-ui-css/semantic.css"
+import { hourMinuteToSeconds, secondsToHourMinuteSecond} from "./utils";
+import Overlay from "./Overlay";
+
+
+const Countdown = (props) => {
+    const eventInSeconds = hourMinuteToSeconds(props.hour, props.minute);
+    const nowInSeconds = hourMinuteToSeconds(props.timeNow.hour, props.timeNow.minute) + props.timeNow.seconds;
+
+    const diff = (eventInSeconds - nowInSeconds);
+    const diffText = diff > 0 ? secondsToHourMinuteSecond(diff) : "tomorrow";
+
+    return (
+    <div className="countdown">
+        <strong>{props.name}</strong> - {diffText}
+        <div className="countdown__icons">
+            <i className="icon edit" onClick={() => props.onEditInit(props.id)}></i>
+            <i className="icon times" onClick={() => props.onRemove(props.id)}></i>
+        </div>
+        <Overlay>
+            <h1>{props.name}</h1>
+            <p>{props.hour.toString().padStart(2,0)}:{props.minute.toString().padStart(2, 0)}</p>
+        </Overlay>
+    </div>
+)};
+
+Countdown.propTypes = {
+    name: PropTypes.string,
+    hour: PropTypes.number,
+    minute: PropTypes.number,
+    onEditInit: PropTypes.func,
+    timeNow: PropTypes.shape({
+        hour: PropTypes.number,
+        minute: PropTypes.number,
+        seconds: PropTypes.number
+    }),
+    onRemove: PropTypes.func
+}
+
+export default Countdown;
